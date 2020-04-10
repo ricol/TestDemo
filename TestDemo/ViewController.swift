96797c012d5a21b100f3bdc1efc9969fcade1c7f
Ricol Wang <ricol@opensimsim.com>
2020-04-10 16:27:02 +1000

    Initial Commit

---
+++ TestDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestDemo
+//
+//  Created by Ricol Wang on 10/4/20.
+//  Copyright © 2020 Ricol Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
